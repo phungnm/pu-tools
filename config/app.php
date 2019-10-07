5a24608e653e59dfaadae315de7bb252b231e224
Phụng Minh <1512414@student.hcmus.edu.vn>
2019-10-07 16:06:51 +0700

    OK

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/pu-tools'),
+    'url' => env('APP_URL', 'https://pu-tools.herokuapp'),
 
-    'asset_url' => env('ASSET_URL', 'http://localhost/pu-tools/public'),
+    'asset_url' => env('ASSET_URL', 'https://pu-tools.herokuapp/public'),
 
     /*
     |--------------------------------------------------------------------------
